f1896099401448d6fea1d3b0e2e9e056af04dfe0
Jessy-Domingues <jess4960@gmail.com>
2023-04-18 10:14:38 +0200

    résolution pb de route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,18 +19,21 @@ use App\Http\Controllers\WelcomeController;
 
 Route::get('/', [WelcomeController::class, 'home'])->name('home');
 
-Route::get('/blog', [ArticlesController::class, 'index'])->name('blog.index');
-
-
-
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');})->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('articles', ArticlesController::class)->except('index');
+    Route::resource('articles', ArticlesController::class)->except(['index', 'show']);
     
 });
 
+
 require __DIR__.'/auth.php';
+
+
+Route::get('/blog', [ArticlesController::class, 'index'])->name('blog.index');
+Route::get('/articles/{article}', [ArticlesController::class, 'show'])->name('articles.show');
+
+
+
